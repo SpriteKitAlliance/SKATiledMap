6501e94a4e0e6261227b14fb6c5209f2563679d7
Skyler Lauren <swiftmist29@gmail.com>
2015-10-17 12:58:18 -0500

    Made it to parsing layer data (need more coffee for this) planning supporting csv and xml first

--- SKATiledMap/SKATMXParser.swift
+++ SKATiledMap/SKATMXParser.swift
@@ -9,13 +9,35 @@
 import Foundation
 
 let kMap = "map"
+let kTileset = "tileset"
+let kTile = "tile"
+let kImage = "image"
+let kLayer = "layer"
+let kData = "data"
+
 let kProperies = "properties"
 
 class SKATMXParser : NSObject, NSXMLParserDelegate {
     
+    enum ParsingMode{
+       case None
+       case TileSet
+    }
+    
     var parser = NSXMLParser()
-    var elements = [String : AnyObject]()
-    var elementName = String()
+    var mapDictionary = [String : AnyObject]()
+
+    var tileSets = [[String: AnyObject]]()
+    var tileSet = [String: AnyObject]()
+    
+    var tileID = ""
+    var tiles = [[String: AnyObject]]()
+    var tile = [String: AnyObject]()
+    
+    var layers = [[String: AnyObject]]()
+    var layer = [String: AnyObject]()
+    
+    var data = [String : AnyObject]()
     
     init(filePath : String){
         
@@ -43,23 +65,94 @@ class SKATMXParser : NSObject, NSXMLParserDelegate {
     
     func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
         
-        self.elementName = elementName
 
         switch elementName{
             case kMap :
-                //TODO parse instead of resetting just in case we don't get it first
-                elements = attributeDict
-                print(elements)
+                for (key, value) in attributeDict{
+                    mapDictionary[key] = value
+                }
             break
+            
+            case kTileset :
+                tileSet = attributeDict
+            break
+            
+            case kTile :
+                
+                if(tileID != ""){
+                    print("THIS IS UNEXPECTED")
+                }
+                for (_, value) in attributeDict{
+                    tileID = value
+                }
+            break
+            
+            case kImage :
+                if(tileID != ""){
+                    tile = [tileID : attributeDict]
+                }
+            break
+            
+            case kLayer :
+                layer = attributeDict
+            break
+            
+            case kData:
+                data = attributeDict
+            break
+            
+
             
         default :
             print("unexpected name found: \(elementName)")
             break
         }
+    
+    }
+    
+    func parser(parser: NSXMLParser, foundCharacters string: String) {
+        
+        if data.keys.count > 0{
+            print("This will be fun: \(string)")
+        }
     }
     
     func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        
+        switch elementName{
+            case kTileset:
+                if(tiles.count > 0){
+                    tileSet["tiles"] = tiles
+                }
+                tileSets.append(tileSet)
+            break
+            
+            case kTile:
+                tiles.append(tile)
+                tileID = ""
+            break
+            
+            case kLayer:
+                layers.append(layer)
+            break
+            
+            case kData:
+                layer["data"] = data
+                data = [String : AnyObject]()
+            break
+            
+            
+            
+            default:
+                break
+        }
+    }
     
+    func parserDidEndDocument(parser: NSXMLParser) {
+        
+        mapDictionary["tilesets"] = tileSets
+        mapDictionary["layers"] = layers
+        print(mapDictionary)
     }
     
     required init?(coder aDecoder: NSCoder) {
